5958673e155a2137c948fc753b1e9c51f855a4b6
Pavel Ajtkulov <ajtkulov@gmail.com>
2017-08-24 03:35:48 +0400

    -

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,94 @@
 package main
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import spark.SparkUtils
 
+/**
+  * Model
+  *
+  * @param keyword     keyword
+  * @param impressions impressions
+  * @param ctr         ctr
+  * @param cost        cost
+  * @param position    position
+  * @param company     position
+  * @param revenue     revenue
+  */
+case class Model(keyword: String, impressions: Double, ctr: Double, cost: Double, position: Int, company: String, revenue: Double) {
+  def toSubModel: KeywordModel = KeywordModel(keyword)
 
-case class Model(keyword: String, impressions: Int, ctr: Double, cost: Double, position: Int, company: String, revenue: Double)
+  def toSubModelPair: KeyChange = KeyChange(keyword, company)
+}
+
+/**
+  * Submodel for common keywords
+  *
+  * @param keyword keyword
+  */
+case class KeywordModel(keyword: String) {
+  def prettyPrint: String = keyword
+}
+
+/**
+  * Submodel for reaching changes
+  * @param keyword keyword
+  * @param company company
+  */
+case class KeyChange(keyword: String, company: String) {
+  def prettyPrint: String = s"$keyword,$company"
+}
+
+case class Diff(key: KeyChange, db: Set[Model], input: Set[Model]) {
+  def prettyString = s"${key.prettyPrint}: db: ${db.mkString("[", ", ", "]")}, input: ${input.mkString("[", ", ", "]")}"
+}
 
 object Main extends App {
+  def readModel(value: String): Model = {
+    val split = value.split(",")
+    Model(split(0), split(1).toDouble, split(2).toDouble, split(3).toDouble, split(4).toInt, split(5), split(6).toDouble)
+  }
+
   override def main(args: Array[String]): Unit = {
 
-    val data1 = Model("1", 2, 0.3, 0.5, 4, "wer", 0.6)
-    val data2 = Model("2", 2, 0.3, 0.5, 4, "wer", 0.6)
-    val data3 = Model("3", 2, 0.3, 0.5, 4, "wer", 0.6)
+    val header = "Search keyword,Impressions,CTR,Cost,Position,Company,Revenue"
 
-    val data11 = Model("1", 2, 0.3, 0.5, 4, "wer", 0.7)
-    val data21 = Model("2", 2, 0.3, 0.5, 4, "wer", 0.6)
-    val data31 = Model("3", 2, 0.3, 0.5, 4, "wer", 0.6)
-    val data41 = Model("5", 2, 0.3, 0.5, 4, "wer", 0.6)
+    def readFile(fileName: String)(implicit sc: SparkContext): RDD[Model] = {
+      sc.textFile(fileName).filter(x => x != header).map(readModel)
+    }
 
-    val sc = SparkUtils.localSpark
+    SparkUtils.withSpark { implicit sc =>
+      val db = readFile("db.csv")
+      val input = readFile("input.csv")
 
-    val rdd1 = sc.makeRDD(List(data1, data2, data3))
-    val rdd2 = sc.makeRDD(List(data11, data21, data31, data41))
+      println("********")
 
-    println("********")
+      val inputSubModel = input.map(_.toSubModel).distinct()
+      val dbSubModel = db.map(_.toSubModel).distinct()
+      inputSubModel.subtract(dbSubModel).map(_.prettyPrint).saveAsTextFile("newKeywords")
+      dbSubModel.subtract(inputSubModel).map(_.prettyPrint).saveAsTextFile("deletedKeywords")
 
-        val diff = rdd2.subtract(rdd1)
-//
-    val res = diff.collect()
+      val inputSubModelPair = input.map(x => x.toSubModelPair -> x)
+      val dbSubModelPair = db.map(x => x.toSubModelPair -> x)
 
-    println(res.mkString("\n"))
-//    println(sc.makeRDD(List(1,2,3,4,5)).sum())
+      val inputGrouped: RDD[(KeyChange, Iterable[(KeyChange, Model)])] = inputSubModelPair.groupBy(_._1)
+      val dbGrouped: RDD[(KeyChange, Iterable[(KeyChange, Model)])] = dbSubModelPair.groupBy(_._1)
 
-    sc.stop()
+      val join: RDD[(KeyChange, (Iterable[(KeyChange, Model)], Iterable[(KeyChange, Model)]))] = inputGrouped.join(dbGrouped)
+
+      join.flatMap {
+        case (key, (inputCollection, dbCollection)) =>
+          val inputSet = inputCollection.map(_._2).toSet
+          val dbSet = dbCollection.map(_._2).toSet
+
+          if (inputSet.intersect(dbSet).isEmpty) {
+            Some(Diff(key, dbSet, inputSet))
+          } else {
+            None
+          }
+      }.map(_.prettyString).saveAsTextFile("changes")
+
+
+    }
   }
 }
--- src/main/scala/SparkUtils.scala
+++ src/main/scala/SparkUtils.scala
@@ -3,11 +3,14 @@ package spark
 import infrastructure.ConfigUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  * Spark utils
+  */
 object SparkUtils {
   def localSpark: SparkContext = {
     val conf = new SparkConf()
-      .setMaster("local[1]")
-      .setAppName("localTest")
+      .setMaster("local[4]")
+      .setAppName("test")
       .set("spark.local.dir", ConfigUtils.config().getString("spark.tmpdir"))
       .set("spark.executor.memory", ConfigUtils.config().getString("spark.executor.memory"))
     val sc = new SparkContext(conf)
@@ -15,5 +18,12 @@ object SparkUtils {
     sc
   }
 
-
+  def withSpark[A](func: SparkContext => A): A = {
+    val sc = localSpark
+    try {
+      func(sc)
+    } finally {
+      sc.stop()
+    }
+  }
 }
