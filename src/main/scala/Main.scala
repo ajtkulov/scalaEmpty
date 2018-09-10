2642cf16b024623be95883d25c82ab1ca4c7e73c
Pavel Ajtkulov <ajtkulov@gmail.com>
2018-09-11 00:59:33 +0400

    -

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,150 @@
 package main
 
+import java.awt.image.BufferedImage
+
+import javax.imageio.ImageIO
+import main.Handler._
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+case class Color(r: Int, g: Int, b: Int) {}
+
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    println(1)
+    val r = read()
+    val res = selectBasement(r)
+    println(r)
+    println(res)
   }
+}
+
+case class Pos(x: Int, y: Int) {
+  def +(other: Pos): Pos = Pos(x + other.x, y + other.y)
+}
+
+class Image[C](values: Array[Array[C]]) {
+  def apply(x: Int) = values(x)
+  def width: Int = values.length
+  def height: Int = values(0).length
+
+  def map[T: ClassTag](f: C => T): Image[T] = {
+    val ar = Array.ofDim[T](width, height)
+
+    for {
+      x <- 0 until width
+      y <- 0 until height
+    } ar(x)(y) = f(values(x)(y))
+
+    new Image(ar)
+  }
+
+  def center(f: C => Boolean): Pos = {
+    var cx = 0
+    var cy = 0
+    var cnt = 0
+    for {
+      x <- 0 until width
+      y <- 0 until height
+    } if (f(values(x)(y))) {
+      cx = cx + x
+      cy = cy + y
+      cnt = cnt +  1
+    }
+
+    Pos(cx / cnt, cy / cnt)
+  }
+
+  lazy val shifts = List[Pos](Pos(1, 0), Pos(0, 1), Pos(-1, 0), Pos(0, -1))
+
+  def bfs(pos: Pos, f: C => Boolean): (Int, List[Pos]) = {
+    val set = mutable.Set[Pos]()
+    var size = 0
+    var br: Pos = Pos(Int.MaxValue, Int.MaxValue)
+    var rb: Pos = Pos(0, 0)
+    var tl: Pos = Pos(0, 0)
+    var lt: Pos = Pos(Int.MaxValue, Int.MaxValue)
+
+    val q = mutable.Queue[Pos]()
+    set.add(pos)
+    q.enqueue(pos)
+    while (q.nonEmpty) {
+      val cur = q.dequeue()
+      for {
+        shift <- shifts
+      } {
+        val n = cur + shift
+        if (!set.contains(n) && f(values(n.x)(n.y))) {
+          size = size + 1
+
+          set.add(n)
+          q.enqueue(n)
+          if (n.y > tl.y || (n.y == tl.y && n.x < tl.x)) tl = n
+          if (n.y < br.y || (n.y == br.y && n.x > br.x)) br = n
+
+          if (n.x < lt.x || (n.x == lt.x && n.y > lt.y)) lt = n
+          if (n.x > rb.x || (n.x == rb.x && n.y < rb.y)) rb = n
+
+        }
+      }
+
+    }
+
+    (size, List(br, rb, tl, lt))
+  }
+
+}
+
+case class ColorImage(array: Array[Array[Color]]) extends Image[Color](array)
+
+object Handler {
+  type ColorImage = Image[Color]
+
+  implicit class ImplicitImage(f: BufferedImage) {
+    def getColor(x: Int, y: Int): Color = {
+      rgb(f.getRGB(x, y))
+    }
+
+    def getColors: Array[Array[Color]] = {
+      val a = Array.ofDim[Color](f.getWidth, f.getHeight)
+      for {
+        x <- 0 until f.getWidth
+        y <- 0 until f.getHeight
+      } a(x)(y) = getColor(x, y)
+      a
+    }
+  }
+
+  def read(): BufferedImage = {
+    ImageIO.read(new java.io.File("/Users/pavel/down/2.jpg"))
+  }
+
+  def rgb(c: Int): Color = {
+    val red = (c & 0xff0000) / 65536
+    val green = (c & 0xff00) / 256
+    val blue = (c & 0xff)
+    Color(red, green, blue)
+  }
+
+  def isGreen(color: Color): Boolean = {
+    color.g > 120 && color.r < 110 && color.b < 110
+  }
+
+  def selectBasement(f: BufferedImage) = {
+    val img = ColorImage(f.getColors)
+
+    val bool = img.map(isGreen)
+
+    val center = bool.center(identity)
+
+    val rr = Iterator.from(0).map(x => bool.bfs(center + Pos(x, x), identity)).filter {case (a, x) => a > 100}.take(1).toList
+
+
+//    val rr = bool.bfs(center, identity)
+    println(123 )
+
+    println(rr)
+
+  }
+
 }
