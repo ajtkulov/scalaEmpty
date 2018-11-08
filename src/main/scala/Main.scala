f5841826d6bb9cd01b3f665a8f2ffddc439cccb6
Pavel Ajtkulov <pavel.ajtkulov@piano.io>
2018-11-08 16:58:20 +0400

    -

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,39 +1,39 @@
 package main
-import breeze.linalg._, eigSym.EigSym
+
+import com.jujutsu.tsne.barneshut.ParallelBHTsne
+
+import scala.util.Random
+
 
 object Main extends App {
+
+  def parse(value: String): Array[Double] = {
+    value.split(" ").filter(_.nonEmpty).map(_.toDouble)
+  }
+
+  def read(fileName: String): Array[Array[Double]] = {
+    scala.io.Source.fromFile(fileName).getLines().map(parse).toArray
+  }
+
+  case class Some1(v: Int, g: String)
+
   override def main(args: Array[String]): Unit = {
-//    val A = DenseMatrix((9.0,0.0,0.0),(0.0,82.0,0.0),(0.0,0.0,25.0))
-    val A = DenseMatrix((1.0,2.0,3.0),(1.0,1.0,2.0),(4.0,3.0,1.0))
-    val z = A * A.t
+    val tsne = new ParallelBHTsne()
+    import com.jujutsu.utils.TSneUtils
 
+    val X: Array[Array[Double]] = read("/Users/pavel/code/T-SNE-Java/tsne-demos/src/main/resources/datasets/mnist2500_X.txt")
 
+    val r = new Random()
 
-    println(z)
-//    val EigSym(lambda, evs) = eigSym(A)
-//    println(lambda)
-//    println(evs)
+    for {
+      i <- 0 until 2500
+      j <- 0 until 100
+    } X(i)(j) = r.nextDouble()
+
+    val config = TSneUtils.buildConfig(X, 3, 55, 20, 1000)
+
+    val res = tsne.tsne(config)
+
+    println(res.map(_.toList).toList.take(20))
   }
-}
-
-object Tsne {
-  def product(f: List[Double], s: List[Double]): Double = {
-    val z = (f zip s) map {
-      case (a: Double, b: Double) => a * b
-    }
-
-    z.sum
-  }
-
-  def hBeta(d: List[Double], beta: Double = 1.0) = {
-    val p = d.map(x => Math.exp(-x * beta))
-    val sumP = p.sum
-    val h = Math.log(sumP) + beta * product(d, p) / sumP
-    (h, p.map(x => x / sumP))
-  }
-
-  def x2p(x: DenseMatrix[Double], tol: Double = 1e-5, perplexity: Double = 30.0) {
-
-  }
-
 }
