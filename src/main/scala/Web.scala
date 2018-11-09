4d634f44d061bf4191ec7919761443d6162025a6
Pavel Ajtkulov <pavel.ajtkulov@piano.io>
2018-11-09 14:30:54 +0400

    -

--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -20,41 +20,41 @@ object Holder {
   lazy val r: Data = read("/Users/pavel/puzzle/center")
 }
 
-//object WebServer {
-//  implicit val system = ActorSystem("some")
-//  implicit val materializer = ActorMaterializer()
-//  implicit val timeout: akka.util.Timeout = Timeout(100 seconds)
-//  implicit val executionContext = system.dispatcher
-//
-//
-//  def main(args: Array[String]) {
-//
-//    val route =
-//      path("get") {
-//        get {
-//          parameters('idx.as[Int]) { idx =>
-//            onComplete(Future {
-//              Match.oneMatch(idx)(MatchParams.precise)
-//            }) {
-//              case Success(_) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "done"))
-//              case Failure(value) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, value.toString()))
-//            }
-//          }
-//        }
-//      } ~
-//      path("get1") {
-//        get {
-//          parameters('idx.as[Int]) { idx =>
-//            onComplete(Future {
-//              Match.oneMatch(idx)(MatchParams.standard)
-//            }) {
-//              case Success(_) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "done"))
-//              case Failure(value) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, value.toString()))
-//            }
-//          }
-//        }
-//      }
-//
-//    Http().bindAndHandle(route, "localhost", 8080)
-//  }
-//}
+object WebServer {
+  implicit val system = ActorSystem("some")
+  implicit val materializer = ActorMaterializer()
+  implicit val timeout: akka.util.Timeout = Timeout(100 seconds)
+  implicit val executionContext = system.dispatcher
+
+
+  def main(args: Array[String]) {
+
+    val route =
+      path("get") {
+        get {
+          parameters('idx.as[Int]) { idx =>
+            onComplete(Future {
+              Match.oneMatch(idx)(MatchParams.precise)
+            }) {
+              case Success(_) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "done"))
+              case Failure(value) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, value.toString()))
+            }
+          }
+        }
+      } ~
+      path("get1") {
+        get {
+          parameters('idx.as[Int]) { idx =>
+            onComplete(Future {
+              Match.oneMatch(idx)(MatchParams.standard)
+            }) {
+              case Success(_) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "done"))
+              case Failure(value) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, value.toString()))
+            }
+          }
+        }
+      }
+
+    Http().bindAndHandle(route, "localhost", 8080)
+  }
+}
