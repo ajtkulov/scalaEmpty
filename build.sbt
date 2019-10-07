8daf9afa8350eef36cf83363f082e0ef5e7d0aad
Pavel Ajtkulov <pavel.ajtkulov@piano.io>
2019-10-07 17:35:50 +0400

    -

--- build.sbt
+++ build.sbt
@@ -8,6 +8,9 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 
+libraryDependencies += "com.google.guava" % "guava" % "28.1-jre"
+
+
 parallelExecution in Test := false
 
 fork := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,47 @@
 package main
 
+import com.google.common.hash.{BloomFilter, Funnels}
+import scala.collection.mutable
+
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    println(1)
+    val trie = Trie(1e-9, 1000000)
+
+    trie.addWord("microsoft")
+    trie.addWord("microsoft research")
+    trie.addWord("mikrotik")
+
+    println(trie.transitions)
   }
+}
+
+case class Trie(error: Double, wordsAmount: Int) {
+  lazy val charset = com.google.common.base.Charsets.US_ASCII
+  val avgWordLength = 6
+
+  val finalWordsBloom = BloomFilter.create[String](Funnels.stringFunnel(charset), wordsAmount, error)
+  val prefixBloom = BloomFilter.create[String](Funnels.stringFunnel(charset), wordsAmount * avgWordLength, error)
+
+  val transitions: mutable.Map[String, mutable.Set[Char]] = collection.mutable.Map[String, mutable.Set[Char]]()
+
+  def addWord(word: String): Unit = {
+    finalWordsBloom.put(word)
+    for (i <- 1 to word.length) {
+      val prefix = word.take(i)
+      prefixBloom.put(prefix)
+
+      val last = prefix.last
+      val transitionPrefix = prefix.takeRight(5).dropRight(1)
+
+      val set = if (transitions.contains(transitionPrefix)) {
+        transitions(transitionPrefix)
+      } else {
+        mutable.Set[Char]()
+      }
+
+      set.add(last)
+      transitions.put(transitionPrefix, set)
+    }
+  }
+
 }
